2f1500a2ac496ae695f48566c23d5fcd90007016
M Yusril <34540627+Sansekai@users.noreply.github.com>
2025-12-18 12:38:53 +0800

    Change source code purchase link in get-token.js
    Updated the link for purchasing the token generation source code.

--- get-token.js
+++ get-token.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // function getToken() {
 const token = async () => {
     try {
-// Bagi yang mau beli source code auto generate tokennya bisa chat ke wa: https://wa.me/6285157729639
+// Bagi yang mau beli source code auto generate tokennya bisa ke link: https://lynk.id/sansekai/mxd6j2ezmxoe
 // karena API generate token ini sewaktu-waktu bisa dimatikan tanpa pemberitahuan sebelumnya.
     const res = await axios.get("https://dramabox-token.vercel.app/token");
     return res.data;
